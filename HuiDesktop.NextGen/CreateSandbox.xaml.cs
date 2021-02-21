32cc9c64a5208deb81e61d1108e9467119f1cba0
老猫不发威你当我病危 <oldcat@huix.cc>
2021-02-21 17:33:42 +0800

    添加AvalonEdit，画了CreateSandbox的框框

--- HuiDesktop.NextGen/CreateSandbox.xaml.cs
+++ HuiDesktop.NextGen/CreateSandbox.xaml.cs
@@ -10,24 +10,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace HuiDesktop.NextGen
 {
     /// <summary>
-    /// MainWindow.xaml 的交互逻辑
+    /// CreateSandbox.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class CreateSandbox : Window
     {
-        public MainWindow()
+        public CreateSandbox()
         {
             InitializeComponent();
-        }
-
-        private void CreateSandboxButtonClick(object sender, RoutedEventArgs e)
-        {
-            new CreateSandbox().ShowDialog();
+            StartupModules.ItemsSource = new List<string> { "Browser模式（HTTP）", "Browser模式（本地文件）" };
         }
     }
 }
