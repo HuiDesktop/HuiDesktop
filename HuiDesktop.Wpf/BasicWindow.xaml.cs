10c1f95a8442abf82b0aecea9fa5b6a9179b6a95
Pure01Fx <pure01fx@outlook.com>
2021-08-04 13:24:56 +0800

    船新的API

--- HuiDesktop.Wpf/BasicWindow.xaml.cs
+++ HuiDesktop.Wpf/BasicWindow.xaml.cs
@@ -24,7 +24,8 @@ namespace HuiDesktop
     {
         public ChromiumWebBrowser browser;
         public NotifyIcon notifyIcon = new NotifyIcon();
-        private JsApi api;
+        private ShowDevToolsLifeSpanHandler showDevToolsLifeSpanHandler = new ShowDevToolsLifeSpanHandler();
+        private NextGenJsApi api;
 
         public BasicWindow(Package.StartupInfo info)
         {
@@ -57,25 +58,27 @@ namespace HuiDesktop
             notifyIcon.ContextMenu.MenuItems.Add(new System.Windows.Forms.MenuItem("DevTools", ContextMenu_DevTools));
             notifyIcon.ContextMenu.MenuItems.Add(new System.Windows.Forms.MenuItem("刷新", ContextMenu_Reload));
             notifyIcon.ContextMenu.MenuItems.Add(new System.Windows.Forms.MenuItem("设置", ContextMenu_Settings));
+            notifyIcon.ContextMenu.MenuItems.Add(new System.Windows.Forms.MenuItem("调试设置开关", ContextMenu_ChangeConfigDevTools));
             notifyIcon.ContextMenu.MenuItems.Add(new System.Windows.Forms.MenuItem("退出", ContextMenu_Exit));
 
             browser = new ChromiumWebBrowser();
             browser.BrowserSettings = new BrowserSettings { WindowlessFrameRate = 60, LocalStorage = CefState.Enabled };
             AddChild(browser);
 
-            api = new JsApi(this);
+            api = new NextGenJsApi(this);
             browser.RequestHandler = requestHandler;
             browser.MenuHandler = new NullMenuHandler();
-            browser.JavascriptObjectRepository.Settings.LegacyBindingEnabled = true;
-            browser.JavascriptObjectRepository.ResolveObject += (s, e) =>
+            browser.JavascriptObjectRepository.ResolveObject += (sender, e) =>
             {
-                if (e.ObjectName == CefSharp.Internals.JavascriptObjectRepository.LegacyObjects)
+                var repo = e.ObjectRepository;
+                if (e.ObjectName == "_huiDesktopIpcBridge")
                 {
-                    //ADD YOUR LEGACY
-                    e.ObjectRepository.Register(name: "huiDesktop", objectToBind: api, isAsync: false);
+                    repo.NameConverter = new CefSharp.JavascriptBinding.CamelCaseJavascriptNameConverter();
+                    repo.Register("_huiDesktopIpcBridge", api, isAsync: true);
                 }
             };
 
+            browser.LifeSpanHandler = showDevToolsLifeSpanHandler;
             browser.Address = url;
         }
 
@@ -90,6 +93,11 @@ namespace HuiDesktop
             browser.ShowDevTools();
         }
 
+        private void ContextMenu_ChangeConfigDevTools(object sender, EventArgs e)
+        {
+            showDevToolsLifeSpanHandler.showDevTools ^= true;
+        }
+
         private void ContextMenu_Reload(object sender, EventArgs e)
         {
             browser.GetBrowser().Reload();
@@ -97,7 +105,7 @@ namespace HuiDesktop
 
         private void ContextMenu_Settings(object sender, EventArgs e)
         {
-            browser.GetBrowser().MainFrame.ExecuteJavaScriptAsync("requestSettings()", browser.Address);
+            api.InvokeSettings();
         }
 
         private void ContextMenu_Exit(object sender, EventArgs e)
@@ -119,12 +127,32 @@ namespace HuiDesktop
         }
     }
 
+    class ShowDevToolsLifeSpanHandler : CefSharp.Handler.LifeSpanHandler
+    {
+        public volatile bool showDevTools;
+        public volatile uint popupCount = 0;
+
+        protected override void OnAfterCreated(IWebBrowser chromiumWebBrowser, IBrowser browser)
+        {
+            base.OnAfterCreated(chromiumWebBrowser, browser);
+            if (popupCount > 0)
+            {
+                popupCount -= 1;
+                if (showDevTools) browser?.ShowDevTools();
+            }
+        }
+
+        protected override bool OnBeforePopup(IWebBrowser chromiumWebBrowser, IBrowser browser, IFrame frame, string targetUrl, string targetFrameName, WindowOpenDisposition targetDisposition, bool userGesture, IPopupFeatures popupFeatures, IWindowInfo windowInfo, IBrowserSettings browserSettings, ref bool noJavascriptAccess, out IWebBrowser newBrowser)
+        {
+            popupCount += 1;
+            return base.OnBeforePopup(chromiumWebBrowser, browser, frame, targetUrl, targetFrameName, targetDisposition, userGesture, popupFeatures, windowInfo, browserSettings, ref noJavascriptAccess, out newBrowser);
+        }
+    }
+
     static class CefInitialize
     {
         public static void InitializeCefSharp(bool disableBlackList)
         {
-            CefSharpSettings.WcfEnabled = true;
-
             var settings = new CefSettings
             {
                 BrowserSubprocessPath = System.IO.Path.Combine(ApplicationInfo.CefSharpFolder, "CefSharp.BrowserSubprocess.exe"),
@@ -142,102 +170,179 @@ namespace HuiDesktop
         }
     }
 
-    class JsApi
+    class NextGenJsApi
     {
-        public static string CallWithTryCatch(string name)
-            => $"try{{{name}}}catch(e){{}}";
+        private IJavascriptCallback positionChangedCallback, settingCallback;
+        private BasicWindow window;
+        private DragMoveManager dragMoveManager;
 
-        public class EasyBasicWindow
+        public NextGenJsApi(BasicWindow parent)
         {
-            private Window _parent;
-            private double top, left, width, height;
+            window = parent;
+            dragMoveManager = new DragMoveManager(parent, parent.browser);
+            window.LocationChanged += Window_LocationChanged;
+        }
 
-            public EasyBasicWindow(JsApi parent)
+        private void Window_LocationChanged(object sender, EventArgs e)
+        {
+            if (positionChangedCallback?.CanExecute == true)
             {
-                _parent = parent._parent;
-                _parent.LocationChanged += Window_LocationChanged;
-                _parent.SizeChanged += Window_SizeChanged;
-            }
-
-            public double Left
-            {
-                get => left;
-                set => _parent.Dispatcher.Invoke(() => _parent.Left = value);
-            }
-
-            public double Top
-            {
-                get => top;
-                set => _parent.Dispatcher.Invoke(() => _parent.Top = value);
-            }
-
-            public double Width
-            {
-                get => width;
-                set => _parent.Dispatcher.Invoke(() => _parent.Width = value);
-            }
-
-            public double Height
-            {
-                get => height;
-                set => _parent.Dispatcher.Invoke(() => _parent.Height = value);
-            }
-
-            private void Window_LocationChanged(object sender, EventArgs e)
-            {
-                left = Convert.ToInt32((sender as Window).Left);
-                top = Convert.ToInt32((sender as Window).Top);
-            }
-
-            private void Window_SizeChanged(object sender, EventArgs e)
-            {
-                height = Convert.ToInt32((sender as Window).Height);
-                width = Convert.ToInt32((sender as Window).Width);
+                positionChangedCallback.ExecuteAsync(window.Left, window.Top);
             }
         }
 
-        public class EasyWorkingArea
+        public void RegisterWindowPositionListener(IJavascriptCallback callback)
         {
-            public double Top { get; }
-            public double Left { get; }
-            public double Width { get; }
-            public double Height { get; }
+            positionChangedCallback = callback;
+        }
 
-            public EasyWorkingArea()
+        public void SetWindowPosition(double x, double y)
+        {
+            window.Dispatcher.Invoke(() =>
             {
-                Top = SystemParameters.WorkArea.Top;
-                Left = SystemParameters.WorkArea.Left;
-                Width = SystemParameters.WorkArea.Width;
-                Height = SystemParameters.WorkArea.Height;
+                window.Left = x;
+                window.Top = y;
+            });
+        }
+
+        public void SetWindowSize(double w, double h)
+        {
+            window.Dispatcher.Invoke(() =>
+            {
+                window.Width = w;
+                window.Height = h;
+            });
+        }
+
+        public class Position
+        {
+            public double X { get; set; }
+            public double Y { get; set; }
+
+            public Position(double x, double y)
+            {
+                X = x;
+                Y = y;
             }
         }
 
-        public class EasyBasicScreen
+        public class Size
         {
-            public double Width { get; }
-            public double Height { get; }
+            public double Width { get; set; }
+            public double Height { get; set; }
 
-            public EasyBasicScreen()
+            public Size(double x, double y)
             {
+                Width = x;
+                Height = y;
+            }
+        }
+
+        public class ScreenInfo
+        {
+            public double Width { get; set; }
+            public double Height { get; set; }
+            public double AvaliWidth { get; set; }
+            public double AvaliHeight { get; set; }
+            public double AvaliX { get; set; }
+            public double AvaliY { get; set; }
+
+            public ScreenInfo()
+            {
+                AvaliX = SystemParameters.WorkArea.Top;
+                AvaliY = SystemParameters.WorkArea.Left;
+                AvaliWidth = SystemParameters.WorkArea.Width;
+                AvaliHeight = SystemParameters.WorkArea.Height;
                 Width = SystemParameters.PrimaryScreenWidth;
                 Height = SystemParameters.PrimaryScreenHeight;
+            }
+        }
+
+        public Position GetWindowPosition()
+        {
+            double left = 0, top = 0;
+            window.Dispatcher.Invoke(() => { left = window.Left; top = window.Top; });
+            return new Position(left, top);
+        }
+
+        public Size GetWindowSize()
+        {
+            double w = 0, h = 0;
+            window.Dispatcher.Invoke(() => { w = window.Width; h = window.Height; });
+            return new Size(w, h);
+        }
+
+        public ScreenInfo GetScreenInfo()
+        {
+            return new ScreenInfo();
+        }
+
+        public void RegisterDragMoveListener(IJavascriptCallback callback)
+        {
+            dragMoveManager.dragMoveCallback = callback;
+        }
+
+        public void RegisterSetting(IJavascriptCallback callback)
+        {
+            settingCallback = callback;
+        }
+
+        public void InvokeSettings()
+        {
+            if (settingCallback != null && settingCallback.CanExecute)
+            {
+                settingCallback.ExecuteAsync();
+            }
+        }
+
+        public void SetBooleanAttribute(string attr, bool value)
+        {
+            switch (attr)
+            {
+                case "TopMost":
+                    window.Dispatcher.Invoke(() => window.Topmost = value);
+                    break;
+                case "ShowInTaskBar":
+                    window.Dispatcher.Invoke(() => window.ShowInTaskbar = value);
+                    break;
+                case "ClickTransparent":
+                    window.Dispatcher.Invoke(() =>
+                    {
+                        var handle = new System.Windows.Interop.WindowInteropHelper(window).Handle;
+                        var t = Win32Api.GetWindowLong(handle, Win32Api.GWL_EXSTYLE);
+                        if ((((t & Win32Api.WS_EX_TRANSPARENT) != 0) ^ value) == true)
+                        {
+                            t ^= Win32Api.WS_EX_TRANSPARENT;
+                            Win32Api.SetWindowLong(handle, Win32Api.GWL_EXSTYLE, t);
+                        }
+                    });
+                    break;
+                case "DragMoveLeft":
+                    dragMoveManager.canLeft = value;
+                    break;
+                case "DragMoveRight":
+                    dragMoveManager.canRight = value;
+                    break;
             }
         }
 
         private class DragMoveManager
         {
             Window window;
-            ChromiumWebBrowser browser;
             Point point;
             MouseButton button;
             bool busy, joke;
 
             public bool canLeft, canRight;
+            /*  dragMoveCallback(keyType, moveType)
+             *  keyType: 0: left, 1: right
+             *  moveType: 0: click, 1: down, 2: up
+             */
+            public IJavascriptCallback dragMoveCallback;
 
             public DragMoveManager(Window window, ChromiumWebBrowser browser)
             {
                 this.window = window;
-                this.browser = browser;
                 browser.PreviewMouseDown += MouseDown;
                 window.MouseMove += MouseMove;
                 window.MouseUp += MouseUp;
@@ -246,32 +351,42 @@ namespace HuiDesktop
 
             private void MouseUp(object sender, MouseButtonEventArgs e)
             {
-                if (!busy) return;
-                if (button != e.ChangedButton) return;
+                if (button != e.ChangedButton)
+                {
+                    e.Handled = false;
+                    return;
+                }
                 e.Handled = true;
+                if (!busy) return;
                 busy = false;
-                System.Diagnostics.Debug.WriteLine("Up");
-                System.Diagnostics.Debug.Flush();
                 Mouse.Capture(null);
-                browser.GetBrowser().MainFrame.ExecuteJavaScriptAsync(CallWithTryCatch($"huiDesktop_DragMove_OnMouse{button}{(joke ? "Click" : "Up")}()"), browser.Address);
+                if (dragMoveCallback == null || !dragMoveCallback.CanExecute) return;
+                if (button == MouseButton.Left) dragMoveCallback.ExecuteAsync(0, joke ? 0 : 2);
+                else if (button == MouseButton.Right) dragMoveCallback.ExecuteAsync(1, joke ? 0 : 2);
             }
 
             private void MouseDown(object sender, MouseButtonEventArgs e)
             {
-                if (busy) return;
                 switch (e.ChangedButton)
                 {
                     case MouseButton.Left:
-                        if (!canLeft) return;
+                        if (!canLeft)
+                        {
+                            e.Handled = false;
+                            return;
+                        }
                         break;
                     case MouseButton.Right:
-                        if (!canRight) return;
+                        if (!canRight)
+                        {
+                            e.Handled = false;
+                            return;
+                        }
                         break;
                     default: return;
                 }
-                System.Diagnostics.Debug.WriteLine("Down");
-                System.Diagnostics.Debug.Flush();
                 e.Handled = true;
+                if (busy) return;
                 button = e.ChangedButton;
                 point = e.GetPosition(window);
                 Mouse.Capture(window);
@@ -280,8 +395,13 @@ namespace HuiDesktop
 
             private void MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
             {
-                if (!busy) return;
+                if (!((button == MouseButton.Left && e.LeftButton == MouseButtonState.Pressed) || (button == MouseButton.Right && e.RightButton == MouseButtonState.Pressed)))
+                {
+                    e.Handled = false;
+                    return;
+                }
                 e.Handled = true;
+                if (!busy) return;
                 bool pressing;
                 if (button == MouseButton.Left) pressing = e.LeftButton == MouseButtonState.Pressed;
                 else pressing = e.RightButton == MouseButtonState.Pressed;
@@ -291,7 +411,9 @@ namespace HuiDesktop
                     if (joke && (vector.X > 3 || vector.Y > 3 || vector.X < -3 || vector.Y < -3))
                     {
                         joke = false;
-                        browser.GetBrowser().MainFrame.ExecuteJavaScriptAsync(CallWithTryCatch($"huiDesktop_DragMove_OnMouse{button}Down()"), browser.Address);
+                        if (dragMoveCallback == null || !dragMoveCallback.CanExecute) return;
+                        if (button == MouseButton.Left) dragMoveCallback.ExecuteAsync(0, 1);
+                        else if (button == MouseButton.Right) dragMoveCallback.ExecuteAsync(1, 1);
                     }
                     if (!joke)
                     {
@@ -303,44 +425,11 @@ namespace HuiDesktop
                 {
                     busy = false;
                     Mouse.Capture(null);
-                    browser.GetBrowser().MainFrame.ExecuteJavaScriptAsync(CallWithTryCatch($"huiDesktop_DragMove_OnMouse{button}{(joke ? "Click" : "Up")}()"), browser.Address);
+                    if (dragMoveCallback == null || !dragMoveCallback.CanExecute) return;
+                    if (button == MouseButton.Left) dragMoveCallback.ExecuteAsync(0, joke ? 0 : 2);
+                    else if (button == MouseButton.Right) dragMoveCallback.ExecuteAsync(1, joke ? 0 : 2);
                 }
             }
-        }
-
-        private BasicWindow _parent;
-        private DragMoveManager _dragMoveManager;
-        public EasyBasicWindow Window { get; }
-        public EasyWorkingArea WorkingArea { get; }
-        public EasyBasicScreen Screen { get; }
-
-        public JsApi(BasicWindow parent)
-        {
-            _parent = parent;
-            _dragMoveManager = new DragMoveManager(_parent, _parent.browser);
-            Window = new EasyBasicWindow(this);
-            WorkingArea = new EasyWorkingArea();
-            Screen = new EasyBasicScreen();
-        }
-
-        public int ApiVersion => 1;
-
-        public bool TopMost { set => _parent.Dispatcher.Invoke(() => _parent.Topmost = value); }
-        public bool DragMoveLeft { get => _dragMoveManager.canLeft; set => _dragMoveManager.canLeft = value; }
-        public bool DragMoveRight { get => _dragMoveManager.canRight; set => _dragMoveManager.canRight = value; }
-        public bool ShowInTaskbar { set => _parent.Dispatcher.Invoke(() => _parent.ShowInTaskbar = value); }
-        public bool ClickTransparent
-        {
-            set => _parent.Dispatcher.Invoke(() =>
-              {
-                  var handle = new System.Windows.Interop.WindowInteropHelper(_parent).Handle;
-                  var t = Win32Api.GetWindowLong(handle, Win32Api.GWL_EXSTYLE);
-                  if ((((t & Win32Api.WS_EX_TRANSPARENT) != 0) ^ value) == true)
-                  {
-                      t ^= Win32Api.WS_EX_TRANSPARENT;
-                      Win32Api.SetWindowLong(handle, Win32Api.GWL_EXSTYLE, t);
-                  }
-              });
         }
     }
 
